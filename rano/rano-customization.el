8c1955ba67ab3a4be6c755b4e2e034c1ec5b0152
valerio <valerio@tropicaliceland.local>
2014-05-12 12:03:20 +0200

    Moving the powerline customization here, and disabling it since it cannot be serialized with the new desktop-save-mode and it messes all up when tried to be deserialized

--- rano/rano-customization.el
+++ rano/rano-customization.el
@@ -3,14 +3,41 @@
 ;; look&feel customizations
 
 ;;override theme (must come after package initialization
-;;(setq solarized-high-contrast-mode-line t)
+(setq solarized-high-contrast-mode-line t)
 (setq solarized-distinct-fringe-background t)
 (setq solarized-use-more-italic t)
-(load-theme 'solarized-dark t)
+ (load-theme 'solarized-dark t)
+
+;;(load-theme 'solarized-light t)
+;;(load-theme 'misterioso t)
+;; (load-theme 'zenburn t)
+
+;; (setq powerline-color1 "#073642")
+;; (setq powerline-color2 "#002b36")
+
+;; (set-face-attribute 'mode-line nil
+;;                     :foreground "#fdf6e3"
+;;                     :background "#2aa198"
+;;                     :box nil)
+;; (set-face-attribute 'mode-line-inactive nil
+;;                     :box nil)
+
+;; ;; powerline
+;; (require 'powerline)
+;; ;; ;;(setq powerline-default-separator 'curve)
+;; ;; ;; (powerline-reset)
+;; ;; ;;(powerline-default-theme)
+;; (powerline-center-theme)
+
+;; flycheck-color-mode-line
+(require 'flycheck-color-mode-line)
+(eval-after-load "flycheck"
+  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
 ;; custom-font
 ;(add-to-list 'default-frame-alist '(font . "Inconsolata 14"))
 					;(set-frame-font "Inconsolata 13")
+					;(set-face-attribute 'default nil :font "Inconsolata-13")
 (set-face-attribute 'default nil :font "Inconsolata-13")
 
 ;; line numbers
@@ -34,12 +61,6 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 
-;; Save all tempfiles in $TMPDIR/emacs$UID/                    
-(defconst emacs-tmp-dir (format "%s/%s%s/"
-				temporary-file-directory "emacs" (user-uid)))
-(setq backup-directory-alist `((".*" . ,emacs-tmp-dir)))
-(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
-(setq auto-save-list-file-prefix  emacs-tmp-dir)
 
 ;; Disable abbrev save to prevent weird annoying
 (setq abbrev-file-name "~/.emacs.d/abbrev_defs")
@@ -61,8 +82,14 @@
 (delete-selection-mode 1)
 
 ;; Forcing show-paren-mode
-(show-paren-mode t)
-(setq show-paren-style 'parentheses)
+(require 'paren)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode +1)
+
+					;(setq show-paren-style 'parentheses)
+;(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+;(setq blink-matching-paren t)
+
 
 ;; visible bell instead of alarm
 (setq visible-bell t)
